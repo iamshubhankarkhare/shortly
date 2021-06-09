46c2e3128bfb8884b3a3a69010235481ccea28a3
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2021-06-09 20:39:47 +0530

    add error handling

--- src/components/inputBlock.js
+++ src/components/inputBlock.js
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
 import bgShortenDesktop from '../assests/bg-shorten-desktop.svg';
-import { Flex, Link, Button, H1, Input } from './styledComponents';
+import { Flex, P, Button, H1, Input } from './styledComponents';
 import axios from 'axios';
 
 function InputBlock() {
   const [value, setValue] = useState('');
+  const [infoText, setInfotext] = useState('');
+  const errText = 'Please add a link';
   const handleOnChange = (e) => {
     setValue(e.target.value);
   };
 
   const handleClick = () => {
     console.log(value);
-    const url = `https://api.shrtco.de/v2/shorten?url=${value}`;
-    const getData = async () => {
-      const res = await axios.get(url);
-      const {
-        data: { result },
-      } = res;
-      console.log(result);
-    };
-    getData();
+    if (value === '') setInfotext(errText);
+    else {
+      setInfotext('Relax while we shorten the link for you...');
+      const url = `https://api.shrtco.de/v2/shorten?url=${value}`;
+      const getData = async () => {
+        const res = await axios.get(url);
+        const {
+          data: { result },
+        } = res;
+        console.log(result);
+        setInfotext('');
+      };
+      getData();
+    }
   };
   return (
     <Flex bg="#EFF1F7">
       <Flex
         flexDirection={['column', 'row']}
         justifyContent="space-around"
-        alignItems="center"
+        alignItems="flex-start"
         bg="primary.darkViolet"
         backgroundImage={`url(${bgShortenDesktop})`}
         backgroundSize="cover"
@@ -37,19 +44,27 @@ function InputBlock() {
         borderRadius="10px"
         style={{ transform: 'translateY(-50%)' }}
       >
-        <Input
-          border="none"
-          borderRadius="10px"
-          fontSize="24px"
-          px={[0, '20px']}
-          py="15px"
-          mx="4"
-          width="100%"
-          placeholder="Shorten a link here..."
-          onChange={(e) => handleOnChange(e)}
-          value={value}
-        />
-
+        <Flex flexDirection="column" width="100%">
+          <Input
+            borderColor={infoText === errText ? 'secondary.red' : 'transparent'}
+            borderRadius="10px"
+            fontSize="24px"
+            py="15px"
+            width="100%"
+            placeholder="Shorten a link here..."
+            onChange={(e) => handleOnChange(e)}
+            value={value}
+          />
+          {infoText !== '' && (
+            <P
+              mb="0"
+              color={infoText === errText ? 'secondary.red' : 'neutral.gray'}
+              fontStyle="italic"
+            >
+              {infoText}
+            </P>
+          )}
+        </Flex>
         <Button
           fontWeight="700"
           bg="primary.cyan"
@@ -58,7 +73,7 @@ function InputBlock() {
           border="none"
           fontSize="24px"
           mt={[4, 0]}
-          mx="4"
+          mx={[0, 4]}
           py="15px"
           width={['100%', '20vw']}
           onClick={() => handleClick()}
